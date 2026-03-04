85dda15b5bbe7905f22eadd446b224fd71cd26bc
Nguyen Huynh <nhuynh9497@gmail.com>
2026-03-04 22:56:36 +0000

    added dim location and some reusable macros. built dbt for test on gcp.

---
+++ models/intermediate/int_trips_unioned.sql
@@ -0,0 +1,13 @@
+with green_tripdata AS (
+    SELECT * FROM {{ ref('staging_green_tripdata') }}
+),
+yellow_tripdata AS ( 
+     SELECT * FROM {{ ref('staging_yellow_data')}} 
+),
+trips_unioned as (
+    select * from green_tripdata
+    union all
+    select * from yellow_tripdata
+)
+
+select distinct pickup_location_id from trips_unioned
--- models/staging/staging_yellow_data.sql
+++ models/staging/staging_yellow_data.sql
@@ -1,4 +1,4 @@
-with stage_greentrip_data AS 
+with stage_yellow_data AS 
 (
     SELECT 
 --identifier--
@@ -8,14 +8,14 @@ cast(pulocationid as int) as pickup_location_id,
 cast(dolocationid as int) as dropoff_location_id,
 
 --timestamp--
-lpep_pickup_datetime as pickup_datetime,
-lpep_dropoff_datetime as dropoff_datetime,
+tpep_pickup_datetime as pickup_datetime,
+tpep_dropoff_datetime as dropoff_datetime,
 
 --trip info--
 store_and_fwd_flag,
 cast(passenger_count as int) as passenger_count,
 cast(trip_distance as numeric) as trip_distance,
-cast(trip_type as int) as trip_type,
+1 as trip_type,--only street type taxi--
 
 --payment info
 cast(fare_amount as numeric) as fare_amount,
@@ -23,12 +23,13 @@ cast(extra as numeric) as extra,
 cast(mta_tax as numeric) as mta_tax,
 cast(tip_amount as numeric) as tip_amount,
 cast(tolls_amount as numeric) as tolls_amount,
-cast(ehail_fee as numeric) as ehail_fee,
 cast(improvement_surcharge as numeric) as improvement_surcharge,
+0 as ehail_fee, -- yellow taxi always have 0 ehail fee--
 cast(total_amount as numeric) as total_amount,
-{{ safe_cast('payment_type', 'integer') }} as payment_type
-from {{source('raw_data', 'green_tripdata') }}
+cast(payment_type as integer) as payment_type
+
+from {{source('raw_data', 'yellow_tripdata') }}
 )
 
 SELECT * from
-stage_greentrip_data WHERE vendor_id IS NOT NULL
+stage_yellow_data WHERE vendor_id IS NOT NULL
